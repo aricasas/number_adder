57ec1254488fa987998466d3a8b7b31c6eaf4b37
Ari <aricasasn@gmail.com>
2020-09-27 21:54:26 -0500

    Generating random numbers instead of reading from the file since its faster

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rand::rngs;
+use rand::Rng;
+use rand::SeedableRng;
 use std::fs;
 use std::time::Instant;
 
@@ -9,19 +12,35 @@ fn add_numbers(vector: &Vec<u32>) -> u64 {
     x
 }
 
-fn read_numbers_from_file(filename: &str) -> Vec<u32> {
+fn _read_numbers_from_file(filename: &str) -> Vec<u32> {
     let numbers = fs::read_to_string(filename).expect("Couldn't read file");
     numbers
-        .lines() // Split on newlines
-        .map(|number| number.parse::<u32>().unwrap()) // Make the Iterator parse the strings into u64
+        .lines() // Split on newlines, returns an iterator
+        .map(|number| number.parse::<u32>().unwrap()) // Make the iterator parse the strings into u32
         .collect() // Make a Vector from the Iterator
 }
 
+fn generate_numbers(count: usize, range: rand::distributions::Uniform<u32>) -> Vec<u32> {
+    let mut rng = rngs::SmallRng::from_entropy();
+    let mut numbers = Vec::with_capacity(count);
+    for _ in 0..count {
+        numbers.push(rng.sample(&range))
+    }
+
+    numbers
+}
+
 fn main() {
+    const RANDOM_NUMBER_COUNT: usize = 100_000_000;
+
+    // Now generate our own numbers
+    let range = rand::distributions::Uniform::new_inclusive(1, 1000);
+
     let now = Instant::now();
-    let numbers = read_numbers_from_file("numbers/num2.txt");
+    let numbers = generate_numbers(RANDOM_NUMBER_COUNT, range);
     println!(
-        "Getting a Vector from the file took: {}μs",
+        "\n\nGenerating {} random numbers took: {}μs",
+        RANDOM_NUMBER_COUNT,
         now.elapsed().as_micros()
     );
 
