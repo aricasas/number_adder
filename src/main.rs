55371ba40704ce6787b2c1081f2b5a513fd8842f
Ari <aricasasn@gmail.com>
2020-09-29 20:47:44 -0500

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,13 @@ use std::thread;
 use std::time::Instant;
 
 fn add_numbers_not_threaded(list: &[u32]) -> u64 {
-    list.iter()
-        .map(
-            |number| u64::from(*number), // Convert numbers to u64
-        )
-        .sum()
+    // Iterates through all of the numbers and adds them
+    list.iter().map(|number| u64::from(*number)).sum()
 }
 
 fn add_numbers_manual_threads(list: &[u32], max_threads: u32) -> u64 {
     if max_threads == 0 {
+        // If max_threads is 0, don't use threads
         add_numbers_not_threaded(list)
     } else {
         // If it's not zero, it must be a positive number
@@ -34,22 +32,31 @@ fn add_numbers_manual_threads(list: &[u32], max_threads: u32) -> u64 {
         let list_length = list.len();
         let thread_count = cmp::min(list_length / 100_000, max_threads as usize);
 
-        let split_data = if list_length % thread_count != 0 {
-            // If it can't divide all the numbers into equal chunks,
-            list.chunks((list_length / thread_count) + 1)
-        } else {
+        let split_data = if list_length % thread_count == 0 {
             list.chunks(list_length / thread_count)
+        } else {
+            // If it can't divide all the numbers into equal chunks,
+            // add one to ths size we want the chunks to be.
+            // This makes it so the remainder
+            list.chunks((list_length / thread_count) + 1)
         };
 
         let mut children = vec![];
 
         for chunk in split_data {
+            // Rust makes sure you can't use an invalid reference
+            // so it deosn't allow you to borrow the chunk directly
+            // because there's a chance that the list the chunk is
+            // from gets freed. So we have to clone the data.
+            // That makes this function very slow
             let clone = Vec::from(chunk);
             children.push(thread::spawn(move || add_numbers_not_threaded(&clone)))
         }
 
         let mut result = 0;
         for child in children {
+            // Add all of the intermediate results we get
+            // from the other threads
             result += child.join().unwrap();
         }
         result
@@ -57,12 +64,16 @@ fn add_numbers_manual_threads(list: &[u32], max_threads: u32) -> u64 {
 }
 
 fn add_numbers_rayon_threads(list: &[u32]) -> u64 {
+    // Wow. Using rayon sure made it a whole lot easier.
+    // And it's also a lot faster
     list.par_iter()
         .map(|number| u64::from(*number))
         .sum::<u64>()
 }
 
 fn _read_numbers_from_file(filename: &str) -> Vec<u32> {
+    // Reading from the file is a lot slower than generating
+    // our own random numbers at runtime
     let numbers = fs::read_to_string(filename).expect("Couldn't read file");
     numbers
         .lines() // Split on newlines, returns an iterator
@@ -74,6 +85,7 @@ fn generate_numbers(count: usize, range: rand::distributions::Uniform<u32>) -> V
     let mut rng = rngs::SmallRng::from_entropy();
     let mut numbers = Vec::with_capacity(count);
     for _ in 0..count {
+        // Add a number within the range to the vector
         numbers.push(rng.sample(&range))
     }
 
@@ -83,33 +95,43 @@ fn generate_numbers(count: usize, range: rand::distributions::Uniform<u32>) -> V
 fn main() {
     const RANDOM_NUMBER_COUNT: usize = 300_000_000;
 
-    // Now generate our own numbers
+    // The range in which numbers are allowed
+    // All the numbers we generate will be betweem 1 and 1,000,000 inclusive
     let range = rand::distributions::Uniform::new_inclusive(1, 1_000_000);
 
+    // Let's see how long it takes to generate numbers
     let now = Instant::now();
-    let numbers = generate_numbers(RANDOM_NUMBER_COUNT, range);
+    let numbers1 = generate_numbers(RANDOM_NUMBER_COUNT, range);
     println!(
         "Generating {} random numbers took: {}s\n_____",
         RANDOM_NUMBER_COUNT,
         now.elapsed().as_micros() as f64 / 1_000_000.0
     );
 
+    // Now let's see how long it takes add all of them
     let now = Instant::now();
-    println!("Sum is: {}", add_numbers_manual_threads(&numbers, 50));
-    println!(
-        "Adding all the numbers using the threaded function took: {}s\n_____",
-        now.elapsed().as_micros() as f64 / 1_000_000.0
-    );
-
-    let now = Instant::now();
-    println!("Sum is: {}", add_numbers_not_threaded(&numbers));
+    println!("Sum is: {}", add_numbers_not_threaded(&numbers1));
     println!(
         "Adding all the numbers took: {}s\n_____",
         now.elapsed().as_micros() as f64 / 1_000_000.0
     );
 
+    // Now let's see how long it takes add all of them using
+    // the fuction that uses threads that I implemented
+    // In release mode, this one is the slowest because
+    // it has to copy a lot of data
+    // But without optimizations enabled, this one beats the first one
     let now = Instant::now();
-    println!("Sum is: {}", add_numbers_rayon_threads(&numbers));
+    println!("Sum is: {}", add_numbers_manual_threads(&numbers1, 50));
+    println!(
+        "Adding all the numbers using the threaded function took: {}s\n_____",
+        now.elapsed().as_micros() as f64 / 1_000_000.0
+    );
+
+    // Now let's see how long it takes add all of them using
+    // the fuction that uses rayon (this one is the fastest)
+    let now = Instant::now();
+    println!("Sum is: {}", add_numbers_rayon_threads(&numbers1));
     println!(
         "Adding all the numbers using rayon threads took: {}s\n_____",
         now.elapsed().as_micros() as f64 / 1_000_000.0
